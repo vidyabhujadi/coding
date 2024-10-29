75d41a563399acc27dbcc4a05ed3e455d092aa15
vidyabhujadi <bhujadividya@gmail.com>
2024-10-29 13:05:10 +0530

    dialog box added

---
+++ script.js
@@ -0,0 +1,9 @@
+function openModel(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display= 'flex';
+}
+
+function closeModel(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display= 'none';
+}
